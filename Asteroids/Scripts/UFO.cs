017f7c447895b89e790ebdb841e0f306d128b826
Miguel Matos <78162554+MiguelMatos98@users.noreply.github.com>
2026-01-22 14:45:04 +0000

    Last Update to repo before Le-Pizza Runner code is also added to the repo

---
+++ Asteroids/Scripts/UFO.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour
+{
+    public float shootInterval;
+    public float shootSpeed;
+    public float ufoSpeed;
+	private float currentShootInterval;
+
+	private Vector2 direction;
+
+	private Rigidbody2D rigidBody;
+
+	public GameObject ufoBullet;
+
+	private GameObject player;
+
+	void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+
+		rigidBody = GetComponent<Rigidbody2D>();
+
+		direction.x = Random.Range(-1.0f, 1.0f);
+		direction.y = Random.Range(-1.0f, 1.0f);
+
+		rigidBody.linearVelocity = direction.normalized * ufoSpeed;
+	}
+
+	void Update()
+    {
+        currentShootInterval += Time.deltaTime;
+        if (currentShootInterval >= shootInterval)
+        {
+            currentShootInterval = 0f;
+
+			Vector2 shootDirection = player.transform.position - transform.position;
+
+			GameObject newBullet = Instantiate(ufoBullet, transform.position, Quaternion.identity);
+			newBullet.GetComponent<Rigidbody2D>().linearVelocity = shootDirection.normalized * shootSpeed;
+			Destroy(newBullet, 10.0f);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.CompareTag("Bullet"))
+		{
+			ScoreManager.instance.UpdateScore(ScoreManager.ScoreType.UFO);
+			gameObject.SetActive(false);
+			collision.gameObject.SetActive(false);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ BattleCity/Scripts/TankMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static RocketScript;
+
+public class TankMovement : MonoBehaviour
+{
+    private float horizontalInput;
+
+	private float verticalInput;
+
+    private Rigidbody2D rigidBody;
+
+    private Animator animator;
+
+    public float speed = 5f;
+
+    public GameObject bullet;
+
+    public float bulletSpeed = 10f;
+
+	public RocketScript.PlayerID playerID;
+
+    public int health;
+
+    private string inputString;
+
+    private KeyCode inputKey;
+
+    public float shootInterval;
+    private float currentShootInterval;
+	private bool canShoot;
+
+	void Awake()
+	{
+        canShoot = true;
+
+		rigidBody = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+
+        if(this.playerID == RocketScript.PlayerID.One)
+        {
+            inputString = "_1";
+			inputKey = KeyCode.Space;
+		}
+		else if (this.playerID == RocketScript.PlayerID.Two)
+		{
+            inputString = "_2";
+            inputKey = KeyCode.Return;
+		}
+	}
+
+	void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal" + inputString);
+        verticalInput = Input.GetAxisRaw("Vertical" + inputString);
+
+        animator.SetBool("IsMoving", horizontalInput != 0 || verticalInput != 0);
+	    
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            Vector3 movement = new Vector3(horizontalInput, verticalInput, 0).normalized;
+			Vector3 up = Vector3.Cross(Vector3.forward, movement);
+			Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+
+        rigidBody.linearVelocity = new Vector3(horizontalInput, verticalInput, 0) * speed;
+
+        currentShootInterval += Time.deltaTime;
+
+		canShoot = (currentShootInterval > shootInterval);
+
+		if (Input.GetKeyDown(inputKey) && canShoot)
+		{
+            currentShootInterval = 0.0f;
+            
+            GameObject newBullet = Instantiate(bullet, transform.GetChild(0).transform.position, transform.rotation);
+            newBullet.GetComponent<RocketScript>().playerID = this.playerID;
+            newBullet.GetComponent<SpriteRenderer>().color = GetComponent<SpriteRenderer>().color;
+            newBullet.GetComponent<Rigidbody2D>().linearVelocity = transform.right * bulletSpeed;
+        }
+	}
+}
